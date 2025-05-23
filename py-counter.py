f2a91af8021c1b6fa90f5edce29e4cff4437de5c
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-05-23 21:01:52 +0200

    Update py-counter.py

--- py-counter.py
+++ py-counter.py
@@ -2,18 +2,16 @@ import tkinter as tk
 import constants as c
 
 
-counter = 0
 
 def increase():
-    global counter
-    counter += 1
-    lbl_counter.config(text=str(counter))
+    value = int(lbl_counter_var.get())
+    lbl_counter_var.set(str(value + 1))
 
 
 def decrease():
-    global counter
-    counter -= 1
-    lbl_counter.config(text=str(counter))
+    value = int(lbl_counter_var.get())
+    value -= 1
+    lbl_counter_var.set(str(value))
 
 
 
@@ -22,30 +20,40 @@ root = tk.Tk()
 root.title('Python Counter')
 root.geometry('600x400')
 
+
 lbl_title = tk.Label(root,
                      text='Py Counter',
                      font=(c.FONT_FAMILY, c.FONT_SIZE_TITLE))
 lbl_title.pack(padx=c.PAD_X_TITLE, pady=c.PAD_Y_TITLE)
 
 
-btn_minus = tk.Button(root, text='-',
+frame_counter = tk.Frame(root)
+frame_counter.pack()
+
+
+btn_minus = tk.Button(frame_counter, text='-',
                       font=(c.FONT_FAMILY, c.FONT_SIZE_TITLE),
                       command=decrease)
-btn_minus.pack(padx=c.PAD_X_TITLE, pady=c.PAD_Y_TITLE,
+btn_minus.pack(padx=c.PAD_Y_TITLE, pady=c.PAD_Y_TITLE,
                ipadx=c.PAD_Y_TITLE, ipady=c.PAD_Y_TITLE,
                side=tk.LEFT)
 
-lbl_counter = tk.Label(root,
-                       text='0',
-                       font=(c.FONT_FAMILY, c.FONT_SIZE_TITLE))
-lbl_counter.pack(padx=c.PAD_X_TITLE, pady=c.PAD_Y_TITLE)
 
-btn_plus = tk.Button(root, text='+',
+lbl_counter_var = tk.StringVar(value='0')
+lbl_counter = tk.Label(frame_counter,
+                       textvariable=lbl_counter_var,
+                       font=(c.FONT_FAMILY, c.FONT_SIZE_TITLE))
+lbl_counter.pack(padx=c.PAD_Y_TITLE, pady=c.PAD_Y_TITLE,
+                 ipadx=c.PAD_Y_TITLE, ipady=c.PAD_Y_TITLE,
+                 side=tk.LEFT)
+
+
+btn_plus = tk.Button(frame_counter, text='+',
                      font=(c.FONT_FAMILY, c.FONT_SIZE_TITLE),
                      command=increase)
-btn_plus.pack(padx=c.PAD_X_TITLE, pady=c.PAD_Y_TITLE,
+btn_plus.pack(padx=c.PAD_Y_TITLE, pady=c.PAD_Y_TITLE,
               ipadx=c.PAD_Y_TITLE, ipady=c.PAD_Y_TITLE,
-              side=tk.RIGHT)
+              side=tk.LEFT)
 
 
 
